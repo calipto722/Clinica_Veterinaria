886c5e58288a3dfc2c35bdcd0f1ac66661026215
calipto722 <141640050+calipto722@users.noreply.github.com>
2023-10-02 12:36:01 -0300

    cliente data

---
+++ src/Acceso_Base_de_Datos/ClienteData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acceso_Base_de_Datos;
+
+import Entidades.Cliente;
+
+/**
+ *
+ * @author noelia
+ */
+public class ClienteData {
+    Cliente cliente= new Cliente();
+}
