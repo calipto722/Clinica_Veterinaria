bddfc8da03325cd0c9d5f7d85612db1888fb7fd4
calipto722 <141640050+calipto722@users.noreply.github.com>
2023-10-05 12:26:18 -0300

    Data

--- src/Acceso_Base_de_Datos/MascotaData.java
+++ src/Acceso_Base_de_Datos/MascotaData.java
@@ -5,6 +5,7 @@
  */
 package Acceso_Base_de_Datos;
 
+import Entidades.Cliente;
 import Entidades.Mascota;
 import java.sql.Connection;
 import java.sql.Date;
@@ -12,12 +13,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author noelia
- */
+
 public class MascotaData {
     Mascota mascota= new Mascota();
     private Connection con= null;
@@ -49,6 +51,68 @@ public class MascotaData {
        }
    }
    public void ModificarMascota(Mascota mascota){
+       String sql="UPDATE `mascota` SET `alias`=?,`sexo`=?,`especie`=?,`raza`=?,`colorPelo`=?,`fechaNac`=?,`idCliente`=?,`activo`=? WHERE `idMascota`=?";
        
+      PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, mascota.getNombreAlias());
+            ps.setString(2, mascota.getSexo());
+            ps.setString(3, mascota.getEspecie());
+            ps.setString(4, mascota.getRaza());
+            ps.setString(5, mascota.getColorPelo());
+            ps.setDate(6,Date.valueOf(mascota.getFechaNacimiento()) );
+            ps.setInt(7,mascota.getcliente().getIdCliente());
+            ps.setBoolean(8,mascota.isEstadoMascota());
+            
+            ps.setInt(8, mascota.getIdMascota());
+        } catch (SQLException ex) {
+            Logger.getLogger(MascotaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+   }
+   public void eliminarMascota(int id){
+       String sql= "UPDATE `mascota` SET `activo`=0 WHERE `idMascota`=?";
+        try {
+            PreparedStatement ps =con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila=ps.executeUpdate();
+            if (fila == 1) {
+                              JOptionPane.showMessageDialog(null, "Se elimino la Mascota. ");
+            }ps.close();
+        } catch (SQLException ex) {
+                       JOptionPane.showMessageDialog(null,"ERROR al acceder a la tabla Mascota "+ ex.getMessage());
+ 
+        }
+       
+   }
+   public List <Mascota> listarMascota(){
+       List<Mascota> mascotas=new ArrayList<>();
+       String sql= "SELECT*FROM mascota WHERE activo=1 ";
+          
+       PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()) {                
+                Mascota mascota = new Mascota();
+                Cliente cliente =new Cliente();
+                ClienteData clienteData=new ClienteData();
+                cliente=clienteData.buscarCliente(rs.getInt("idCliente"));
+                mascota.setColorPelo(rs.getString("colorPelo"));
+                mascota.setEspecie(rs.getString("especie"));
+                mascota.setEstadoMascota(rs.getBoolean("activo"));
+                mascota.setFechaNacimiento(rs.getDate("fechaNac").toLocalDate());
+                mascota.setIdMascota(rs.getInt("idMascota"));
+                mascota.setNombreAlias(rs.getString("alias"));
+                mascota.setRaza(rs.getString("raza"));
+                mascota.setSexo(rs.getString("sexo"));
+                mascotas.add(mascota);
+            }ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(MascotaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+            return mascotas;
    }
 }
